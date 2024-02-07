397676b5a1d71a8f0f57e57c1153975616df55b8
RooseMalen008 <roosemalen.yasser@gmail.com>
2024-02-07 12:33:02 -0800

    fix

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -5,13 +5,13 @@
  *
  * Return: Always 0 (success)
  */
+
 int main(void)
 {
         char i;
 
         for (i = 'a'; i <= 'z'; i++)
             putchar(i);
-
         putchar('\n');
         return (0);
 }
