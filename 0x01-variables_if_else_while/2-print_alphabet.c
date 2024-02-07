b1324bd207c9956861136cf5119da69c06a1612a
RooseMalen008 <roosemalen.yasser@gmail.com>
2024-02-07 12:30:14 -0800

    Fix TAsk

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+
 /**
  * main - Entry point
  *
@@ -8,7 +9,7 @@ int main(void)
 {
         char i;
 
-        for (i = 'a' ; i <= 'z'  ; i++)
+        for (i = 'a'; i <= 'z'; i++)
             putchar(i);
 
         putchar('\n');
