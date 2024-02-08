0b2731757b6be321b92081f75b9507870a8f10cb
RooseMalen008 <roosemalen.yasser@gmail.com>
2024-02-08 10:32:48 -0800

    Tasks

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,17 +2,21 @@
 
 /**
  * print_alphabet_x10 - prints the alphabet 10 times
+ * Return : void
  */
+
 void print_alphabet_x10(void)
 {
-    int i, j;
-
-    for (i = 0; i < 10; i++)
-    {
-        for (j = 'a'; j <= 'z'; j++)
-        {
-            _putchar(j);
-        }
-        _putchar('\n');
-    }
+	char ch;
+	int i;
+	i = 0;
+	while (i < 10)
+	{
+		for (ch = 'a'; ch <= 'z'; ch++)
+		{
+			_putchar(ch);
+		}
+		_putchar('\n');
+		i++;
+	}
 }
