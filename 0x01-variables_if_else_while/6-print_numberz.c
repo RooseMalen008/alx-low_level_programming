43f6d075278ddeb36fab1d4178bff71b8278e969
RooseMalen008 <roosemalen.yasser@gmail.com>
2024-02-07 17:38:45 -0800

    6-Fix Numberz

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -7,17 +7,10 @@
  */
 int main(void)
 {
-    putchar('0');
-    putchar('1' - '0');
-    putchar('2' - '0');
-    putchar('3' - '0');
-    putchar('4' - '0');
-    putchar('5' - '0');
-    putchar('6' - '0');
-    putchar('7' - '0');
-    putchar('8' - '0');
-    putchar('9' - '0');
+	int i;
 
-    putchar('\n');
-    return (0);
+	for (i = 0; i < 10; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
 }
